238f10fe19bc108bc054c2e441b9a20c19181185
Bashar-Owainat <owainatbashar13@gmail.com>
2022-04-19 06:09:40 +0300

    chall 12 animal shelter

---
+++ DataStructureApp/DataStructureApp/Chall-12-stack-queue-animal-shelter/AnimalShelter.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureApp.Chall_12_stack_queue_animal_shelter
+{
+    public class AnimalShelter
+    {
+        public AnimalNode front;
+        public AnimalNode rear;
+
+        public AnimalShelter()
+        {
+            this.front = this.rear = null;
+
+        }
+
+        public bool IsEmpty()
+        {
+            if (front == null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void Enqueue(Animal value)
+        {
+            AnimalNode newNode = new AnimalNode(value);
+            if (rear == null)
+            {
+                rear = front = newNode;
+            }
+            else
+            {
+                rear.next = newNode;
+                rear = newNode;
+            }
+        }
+
+        public string Dequeue()
+        {
+            if (IsEmpty())
+            {
+                throw new InvalidOperationException("You can't Peek or dequeue on an empty queue!");
+
+            }
+
+            AnimalNode temp = this.front;
+            this.front = this.front.next;
+            temp.next = null;
+
+            if (this.front == null)
+            {
+                this.rear = null;
+            }
+
+            return temp.value.name;
+
+        }
+    }
+}
---
+++ DataStructureApp/DataStructureApp/Chall-12-stack-queue-animal-shelter/Cat.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureApp.Chall_12_stack_queue_animal_shelter
+{
+    public class Cat : Animal
+    {
+       public Cat(string name): base(name)
+        {
+           // this.name = name;
+        }
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
